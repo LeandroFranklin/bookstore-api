97571e5f86bc1312b2df97c6117895a32af5b23d
Leandro Franklin Lima de Farias <leandrofrankss5@gmail.com>
2022-05-04 19:37:49 -0300

    Criando meus repositories

---
+++ src/main/java/com/leandro/bookstore/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.leandro.bookstore.repositories;
+
+import com.leandro.domain.Categoria;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+    
+}
---
+++ src/main/java/com/leandro/bookstore/repositories/LivroRepository.java
@@ -0,0 +1,11 @@
+package com.leandro.bookstore.repositories;
+
+import com.leandro.domain.Livro;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
+    
+}
